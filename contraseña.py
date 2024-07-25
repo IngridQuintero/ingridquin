ff804d2d9f5dfd1c2332f851df0eb2a1769848a5
IngridQuintero <164368515+IngridQuintero@users.noreply.github.com>
2024-07-25 11:04:28 -0300

    Add files via upload

---
+++ contraseña.py
@@ -0,0 +1,14 @@
+contra= input("ingrese")
+listap=[]
+
+for i in contra:
+        i= "*"
+        print (i, end="")
+        listap.append(i)
+print()
+print(listap)
+listap.pop(2)
+print(listap)
+
+
+   
